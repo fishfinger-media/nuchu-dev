a0320f4035f01c2a616140a05095c5b66ddb00bb
Rahuli20 <111457292+Rahuli20@users.noreply.github.com>
2024-02-25 23:20:07 +0000

    GSAP Animations

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
-import './styles/style.css'
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-console.log('Hello')
+gsap.registerPlugin(ScrollTrigger);
+
+// WAVE TEXT ANIMATION
+
+// wave-text has an attribute of startOffset, this is moved from 0 to -50% as the user scrolls down. 
+    gsap.to("#wave-text", {
+        attr: {startOffset: "-50%"},
+        scrollTrigger: {
+            trigger: ".section_wave",
+            scrub: 1,
+        }
+    });
+
+
+gsap.to(".nav_logo",{
+    height: "2.5rem",
+    scrollTrigger: {
+        trigger: ".header",
+        start: "bottom top",
+        end: "bottom -10vw",
+        scrub: 1,
+    }
+
+})
